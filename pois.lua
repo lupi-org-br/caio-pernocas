7cae4272dfb73e2e2dc8648e3defefb109973bef
lino <luizgustavolino@gmail.com>
2026-03-20 13:14:50 -0300

    .

--- pois.lua
+++ pois.lua
@@ -243,11 +243,10 @@ local function make_bird(bird)
 end
 
 local function make_decal(decal, name, width_tiles, height_tiles)
-    local rx, ry
+    local rx, ry = ((1 + decal.x - (width_tiles-1)) * 16), ((1 + decal.y - (height_tiles-1)) * 16)
     local sprite_data = Sprites.poi[name].decal
 
     local function update_relative_position(camx, camy)
-        rx, ry = ((1 + decal.x - (width_tiles-1)) * 16), ((1 + decal.y - (height_tiles-1)) * 16)
         return true
     end
 
