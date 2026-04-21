3fd0ed6c6f87ec0eddab6daad70c3d58f692f8a3
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-04-21 14:27:54 -0300

    .

--- poi/branch/branch.lua
+++ poi/branch/branch.lua
@@ -1,4 +1,5 @@
 function make_branch(branch, name)
+    -- branches is a 4x2 tilemap
     local wx = branch.x * 16 - (4 * 16)
     local wy = branch.y * 16 - (2 * 16)
     local sprite_data = Sprites.poi.branch[tostring(1)]
@@ -9,7 +10,11 @@ function make_branch(branch, name)
 
     return {
         update_relative_position = update_relative_position,
-        before_frame = function(frame, player, map, camera) end,
+        before_frame = function(frame, player, map, camera)
+            for x = 0, 3 do
+                map.set_colide(wx + x * 16, wy, 11)
+            end
+        end,
         on_frame = function(frame, player, map, camera)
             ui.spr(sprite_data, wx, wy)
         end,
