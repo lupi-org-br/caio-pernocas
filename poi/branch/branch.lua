06f0c52ac94df68cbaab4d9b71f53544ff56c128
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-04-21 16:59:31 -0300

    .

--- poi/branch/branch.lua
+++ poi/branch/branch.lua
@@ -1,6 +1,6 @@
 function make_branch(branch, name)
     -- branches is a 4x2 tilemap
-    local dx = name == 'left' and 64 or 1
+    local dx = name == 'left' and 58 or 1
     local flipped = not (name == 'left' and true or false)
     local wx = branch.x * 16 - dx
     local wy = branch.y * 16 - (1 * 16)
