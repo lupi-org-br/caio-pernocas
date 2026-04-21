b99206fa70b6f54114e36dad8c79d84e0eb0cd9c
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-04-21 16:33:12 -0300

    .

--- poi/branch/branch.lua
+++ poi/branch/branch.lua
@@ -20,7 +20,7 @@ function make_branch(branch, name)
         end,
         on_frame = function(frame, player, map, camera)
             local flipped = name == 'left' and false or true
-            ui.spr(sprite_data, wx, wy, flipped, false)
+            ui.spr(sprite_data, wx, wy, true, false)
         end,
     }
 end
