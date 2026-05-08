0a0619759955eb611dd1a70ed833db5944746d78
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-05-08 11:23:03 -0300

    .

--- ui.lua
+++ ui.lua
@@ -1,4 +1,5 @@
 function make_ui(player_ref)
+    local current_points = 0
     return {
         before_frame = function(frame, camera, player, map) end,
         on_frame = function(frame, camera, player, map)
@@ -6,17 +7,22 @@ function make_ui(player_ref)
             ui.print(timer, 1, 1, kColors.black)
             ui.print(timer, 0, 0, kColors.yellow)
             
-            local points = player_ref.get_points()
+            local target_points = player_ref.get_points()
             local total_points = player_ref.get_total_points()
             
             if total_points <= 0 then return end 
             
-            local top = 16 + 40 - (40 * points / total_points) // 1
+            current_points = current_points + (target_points - current_points) * 0.1
+            if math.abs(target_points - current_points) < 0.01 then
+                current_points = target_points
+            end
+            
+            local top = 16 + 40 - (40 * current_points / total_points) // 1
             ui.rect(3, 15, 11, 59, kColors.black)
             ui.rect(2, 14, 10, 58, kColors.yellow)
             ui.rectfill(3, 15, 9, 57, kColors.purple_dark)
             
-            if points > 0 and points ~= total_points then
+            if current_points > 0 and current_points < total_points then
                 ui.circfill(6 + math.floor(math.cos(frame / 12) + 0.5), top, 2, kColors.purple_light)
                 ui.circfill(6 + math.floor(math.sin(frame / 12) + 0.5), top, 2, kColors.red_light)
             end
