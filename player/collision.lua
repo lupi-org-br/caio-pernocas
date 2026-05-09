c8c5947a4912a85e845f3095fcf1ea6bde05119c
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-05-09 00:11:42 -0300

    .

--- player/collision.lua
+++ player/collision.lua
@@ -52,6 +52,11 @@ function check_edge_colisions(ctx)
     local right_foot = ctx.map_ref.colides(ctx.position.x + 27, check_y, kColisionType.bottom)
     local center = ctx.map_ref.colides(ctx.position.x + 16, check_y, kColisionType.bottom)
 
+    -- Debug visualization
+    ui.circfill(ctx.position.x + 5, check_y, 1, 2) -- red_light
+    ui.circfill(ctx.position.x + 27, check_y, 1, 2) -- red_light
+    ui.circfill(ctx.position.x + 16, check_y, 1, 63) -- yellow
+
     if not left_foot and center then
         return true, true
     elseif not right_foot and center then
