c705550b7293577a6c850a0c284039663ad2c010
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-05-20 12:06:12 -0300

    .

--- poi/penpen/penpen.lua
+++ poi/penpen/penpen.lua
@@ -2,6 +2,13 @@ function make_penpen(data)
     current_frame = 1
     local wx = data.x * 16
     local wy = data.y * 16
+    local animations = {
+        ["idle"] = {frames = 4, cadency = 8, sprite = "walk"},
+        ["nearby"] = {frames = 4, cadency = 4, sprite = "walk"},
+        ["slide"] = {frames = 2, cadency = 6, sprite = "slide"}
+    }
+
+    local state = "idle"
     
     return {
         update_relative_position = function() return true end,
@@ -15,8 +22,9 @@ function make_penpen(data)
         end,
         
         on_frame = function(frame, player, map, camera)
-            current_frame = (1 + ((frame // 8) % 4)) // 1
-            local sprite_data = Sprites.poi.penpen["walk_" .. math.floor(current_frame)]
+            local animation = animations[state]
+            current_frame = (1 + ((frame // animation.cadency) % animation.frames)) // 1
+            local sprite_data = Sprites.poi.penpen[animation.sprite .. "_" .. math.floor(current_frame)]
             ui.spr(sprite_data, wx, wy, false, false)
         end
     }
