6717a41cb28f1cc8b8cd08cb49f34d563020ec51
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-05-25 23:59:27 -0300

    .

--- const.lua
+++ const.lua
@@ -130,7 +130,7 @@ kStages = {
         bg_up        = Sprites.tilemap.snowy_b_top,
         bg_down      = Sprites.tilemap.snowy_b_bot,
         cherries     = 2,
-        bg_tint      = 233,
+        bg_tint      = 232,
         display_name = "X 2-2",
         map_name     = "maps.stages.w2s2"
     },
