e14b3f0080471cce907a73e33a6d269b55404394
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-09-25 21:10:19 -0300

    .

--- ota.lua
+++ ota.lua
@@ -1,7 +1,9 @@
 function make_ota()
-    return update = function()
-        ui.camera()
-        ui.clip()
-        ui.cls(kColors.purple_light)
-    end 
+    return {
+        update = function()
+            ui.camera()
+            ui.clip()
+            ui.cls(kColors.purple_light)
+        end 
+    }
 end 
